5072c5905f690696ca5e7bdde8fa19365b5794e3
Leaf Ying <leaf.ying.work@gmail.com>
2023-07-22 20:39:50 +0800

    Update train & test scripts to use torchrun latest

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -4,16 +4,18 @@ CONFIG=$1
 GPUS=$2
 NNODES=${NNODES:-1}
 NODE_RANK=${NODE_RANK:-0}
+JOB_ID=${JOB_ID:-0}
 PORT=${PORT:-29500}
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-python -m torch.distributed.launch \
-    --nnodes=$NNODES \
-    --node_rank=$NODE_RANK \
-    --master_addr=$MASTER_ADDR \
-    --nproc_per_node=$GPUS \
-    --master_port=$PORT \
+torchrun \
+    --nnodes $NNODES \
+    --node-rank $NODE_RANK \
+    --nproc-per-node $GPUS \
+    --rdzv-id $JOB_ID \
+    --rdzv-backend "c10d" \
+    --rdzv-endpoint "$MASTER_ADDR:$PORT" \
     $(dirname "$0")/train.py \
     $CONFIG \
     --launcher pytorch ${@:3}
