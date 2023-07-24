5a496057a9f25d7f6020393b9ffef6dab1386bc8
Leaf Ying <leaf.ying.work@gmail.com>
2023-07-25 02:55:17 +0800

    Migrate torch.distributed.run to torchrun

--- tools/dist_test.sh
+++ tools/dist_test.sh
@@ -5,18 +5,20 @@ CHECKPOINT=$2
 GPUS=$3
 NNODES=${NNODES:-1}
 NODE_RANK=${NODE_RANK:-0}
+JOB_ID=${JOB_ID:-0}
 PORT=${PORT:-29500}
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
 PYTHONPATH="$(dirname "$0")/..:$PYTHONPATH" \
-python -m torch.distributed.launch \
-    --nnodes="$NNODES" \
-    --node_rank="$NODE_RANK" \
-    --master_addr="$MASTER_ADDR" \
-    --nproc_per_node="$GPUS" \
-    --master_port="$PORT" \
-    --use-env \
-    "$(dirname "$0")/test.py" \
+torchrun \
+    --nnodes "$NNODES" \
+    --node-rank "$NODE_RANK" \
+    --nproc-per-node "$GPUS" \
+    --rdzv-id "$JOB_ID" \
+    --rdzv-backend "c10d" \
+    --master-addr="$MASTER_ADDR" \
+    --master-port="$PORT" \
+    "$(dirname "$0")/train.py" \
     "$CONFIG" \
     "$CHECKPOINT" \
     --launcher pytorch \
